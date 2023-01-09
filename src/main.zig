4cb5982b4961e8459b000a0f29f65e7cf90a0dcf
Max Gonzih <gonzih@gmail.com>
2023-01-09 12:47:38 -0800

    ffs

--- src/main.zig
+++ src/main.zig
@@ -1,52 +1,89 @@
 const std = @import("std");
 const testing = std.testing;
 const process = std.process;
+const builtin = @import("builtin");
 
 pub const Context = struct {
-    name: []const u8,
+    cmd: []const u8,
 };
 
-pub const Command = struct {
-    name: []const u8,
-    // children: []*Command = &[_]*Command{},
-    run: fn (ctx: *Context) void,
+fn CommandBase(comptime t: bool) type {
+    return struct {
+        const InnerIt = switch (t) {
+            true => process.ArgIteratorGeneral(.{}),
+            false => process.ArgIterator,
+        };
 
-    pub fn exec(comptime self: *Command) void {
-        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-        defer _ = gpa.deinit();
-        var arena = std.heap.ArenaAllocator.init(gpa.allocator());
-        defer arena.deinit();
+        it: InnerIt,
+        cmd: []const u8,
+        // children: []*CommandBase(t) = &[_]*CommandBase(t){},
+        run: fn (ctx: *Context) void,
+        // init: ?fn (ctx: *CommandBase(t)) void = undefined,
+        // allocator: std.mem.Allocator = switch (t) {
+        //     true => testing.allocator,
+        //     false => std.heap.page_allocator,
+        // },
+        allocator: std.mem.Allocator = std.heap.page_allocator,
 
-        var a = arena.allocator();
-        var args = try process.argsWithAllocator(a);
-        defer args.deinit();
+        // pub fn exec(comptime self: *CommandBase(t)) void {
+        //     var args = switch (t) {
+        //         true => try InnerIt.initWithAllocator(self.allocator, "dummy cli args"),
+        //         false => try InnerIt.initWithAllocator(self.allocator),
+        //     };
+        //     // defer args.deinit();
 
-        std.debug.print("args: {any}\n", .{args});
+        //     std.debug.print("args: {any}\n", .{args});
 
-        // skip my own exe name
-        _ = args.skip();
+        //     // skip my own exe name
+        //     // _ = args.skip();
 
-        self.execWith(args);
-    }
+        //     self.execWith(args);
+        // }
 
-    pub fn execWith(comptime self: *Command, args: process.ArgIterator) void {
-        std.debug.print("args: {any}\n", .{args});
-        var ctx = Context{ .name = self.name };
-        self.run(&ctx);
-    }
-};
+        pub fn execWith(comptime self: *CommandBase(t), args: InnerIt) void {
+            std.debug.print("args: {any}\n", .{args});
+            var ctx = Context{ .cmd = self.cmd };
+            self.run(&ctx);
+        }
+
+        fn deinit(comptime self: *CommandBase(t)) void {
+            for (self.children) |child| {
+                child.deinit();
+            }
+            // self.allocator.free(self);
+        }
+    };
+}
+
+pub const Command = CommandBase(false);
+
+var setThis: usize = 0;
 
 fn run(ctx: *Context) void {
+    setThis = 10;
     std.debug.print("running {s}\n", .{ctx.name});
 }
 
-test "basic command functionality" {
-    comptime var cmd = Command{
-        .name = "add",
+test "mocked out command line" {
+    const CommandTestable = CommandBase(true);
+
+    setThis = 0;
+
+    const a = std.testing.allocator;
+
+    var input_cmd_line = "zig add --foo bar --baz 10";
+    var it = try process.ArgIteratorGeneral(.{}).init(a, input_cmd_line);
+    std.debug.print("it: {any}\n", .{it});
+
+    comptime var cmd = CommandTestable{
+        .cmd = "add",
         .run = run,
+        .allocator = a,
     };
 
-    cmd.exec();
+    // defer cmd.deinit();
 
-    try testing.expect(10 == 10);
+    cmd.execWith(it);
+
+    try testing.expect(setThis == 10);
 }
