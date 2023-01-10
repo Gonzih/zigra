6a35b8a252c6c9760314a7c70ef684415751751a
Max Gonzih <gonzih@gmail.com>
2023-01-10 10:28:09 -0800

    Isolate tests

--- src/main.zig
+++ src/main.zig
@@ -68,26 +68,28 @@ fn CommandT(comptime Runner: type) type {
     return CommandBase(args.ArgParser(true), Runner);
 }
 
-const RunnerTone = struct {
-    v: usize = 0,
-
-    pub fn init(ctx: Context) RunnerTone {
-        _ = ctx;
-        return RunnerTone{};
-    }
-
-    pub fn run(self: *RunnerTone, ctx: Context) !void {
-        _ = ctx;
-        self.v = 10;
-        return;
-    }
-
-    pub fn deinit(self: *RunnerTone) void {
-        self.v = 0;
-    }
-};
-
 test "test one" {
+    const RunnerTone = struct {
+        v: usize = 0,
+
+        pub const Self = @This();
+
+        pub fn init(ctx: Context) Self {
+            _ = ctx;
+            return Self{};
+        }
+
+        pub fn run(self: *Self, ctx: Context) !void {
+            _ = ctx;
+            self.v = 10;
+            return;
+        }
+
+        pub fn deinit(self: *Self) void {
+            self.v = 0;
+        }
+    };
+
     const CommandTone = CommandT(RunnerTone);
     var cmd = try CommandTone.initMock(std.testing.allocator, "one", "one -v=10");
     cmd.exec(std.testing.allocator) catch unreachable;
