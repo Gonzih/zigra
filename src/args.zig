a2b309f3cb01df4c9e58ad2af32b4477b91def99
Max Gonzih <gonzih@gmail.com>
2023-01-10 08:54:11 -0800

    Fix args

--- src/args.zig
+++ src/args.zig
@@ -29,24 +29,18 @@ pub fn ArgParser(comptime mock: bool) type {
             var list = ArgsList.init(allocator);
             var map = ArgsMap.init(allocator);
 
-            // TODO rewrite this
-
+            // If argument is a flag, add it to the map
+            // split on '='
+            // If argument is a value, add it to the list
             while (iter.next()) |arg| {
                 if (arg[0] == '-') {
-                    const nxt = iter.next();
-                    var value: []const u8 = "";
-                    if (nxt) |n| {
-                        if (n[0] != '-') {
-                            value = n;
-                        } else {
-                            iter.prev();
-                        }
-                    }
-
-                    std.debug.print("=> Key: \"{s}\", Value: \"{s}\"\n", .{ arg, value });
-                    try map.put(arg, value);
+                    var split = std.mem.split(u8, arg, "=");
+                    const key = split.next().?;
+                    const value = if (split.next()) |v| v else "";
+                    std.debug.print("=> Flag: \"{s}\" = \"{s}\"\n", .{ key, value });
+                    try map.put(key, value);
                 } else {
-                    // std.debug.print("=> Arg: \"{s}\"\n", .{arg});
+                    std.debug.print("=> Value: \"{s}\"\n", .{arg});
                     try list.append(arg);
                 }
             }
@@ -103,7 +97,7 @@ test "check if elements are present" {
 test "check if elements are present" {
     std.debug.print("\n", .{});
     const allocator = std.testing.allocator;
-    var parser = try ArgParser(true).parse(allocator, "binary-path test --bool-arg --string-arg string-value");
+    var parser = try ArgParser(true).parse(allocator, "binary-path test --bool-arg --string-arg=string-value");
     defer parser.deinit();
 
     var asserts = [_][]const u8{
@@ -111,22 +105,24 @@ test "check if elements are present" {
         "test",
     };
 
-    for (parser.args.items) |arg, idx| {
-        const assert = asserts[idx];
-        std.debug.print("=> Comparing: {s} == {s}\n", .{ arg, assert });
-        try testing.expect(std.mem.eql(u8, arg, assert));
+    for (asserts) |assert, idx| {
+        const value = parser.args.items[idx];
+        std.debug.print("=> Comparing: {s} == {s}\n", .{ value, assert });
+        try testing.expect(std.mem.eql(u8, value, assert));
     }
 
     var map_asserts = std.StringHashMap([]const u8).init(allocator);
     map_asserts.put("--bool-arg", "") catch unreachable;
     map_asserts.put("--string-arg", "string-value") catch unreachable;
+    defer map_asserts.deinit();
 
-    // Iterate over the map and check if the values are correct
-    for (map_asserts.entries) |entry| {
-        const key = entry.key;
-        const value = entry.value;
+    var it = map_asserts.iterator();
+    while (it.next()) |entry| {
+        const key = entry.key_ptr.*;
+        const value = entry.value_ptr.*;
+        std.debug.print("=> Found: {s} => {s}\n", .{ key, value });
         const map_value = parser.map.get(key) orelse unreachable;
-        std.debug.print("=> Comparing: {s} == {s}\n", .{ map_value, value });
+        std.debug.print("=> Comparing: \"{s}\" == \"{s}\"\n", .{ map_value, value });
         try testing.expect(std.mem.eql(u8, map_value, value));
     }
 }
